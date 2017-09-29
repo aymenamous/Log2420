02c1345e95033b5c8fb8619e67a7d9230498a2fd
aymenamous <aymenamous@gmail.com>
2017-09-28 22:58:55 -0400

    Organisation fichiers

--- TD2/main.js
+++ TD2/main.js
@@ -2,5 +2,19 @@
 $(document).ready(function(){
 	
 	$(".nav").css("margin-top",$(".menu-horizontal").height()+10);
+	loadCOntent("carteStations.html","carte");
+	loadCOntent("listeStations.html","liste");
 	
 });
+
+function loadCOntent(file,divId){
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			document.getElementById(divId).innerHTML = this.responseText;
+		}
+	};
+	
+	xhttp.open("GET", file, true);
+	xhttp.send();
+}
