3b7896ff39851ec2950cd75deb9538f6efb23801
aymenamous <aymenamous@gmail.com>
2017-10-12 01:02:21 -0400

    Etat station

--- TD2/main.js
+++ TD2/main.js
@@ -5,21 +5,63 @@ $(document).ready(function(){
 	$("#carte").load("carteStations.html");
 	$("#liste").load("listeStations.html");
 	
+	
 });
 
-function activateAutoComplete(){
+var map;
+var marker;
+function activerAutoComplete(){
 	$.getJSON( "https://secure.bixi.com/data/stations.json", function(data) {
 		var stationName = [];
 		$.each( data["stations"], function(key,val ) {
-		stationName.push(val["s"]);
-		
+			stationName.push(val["s"]);
+			
 		});
 		$("#tags" ).autocomplete({
-				source: stationName
-			});
+			source: stationName,
+			select: function(event, ui){
+				$("#stationChoisie").text(ui["item"]["value"]);
+				
+				
+				var pos = {lat: 0, lng: 0};
+				$.each( data["stations"], function(key,val ) {
+					if (val["s"] == ui["item"]["value"] ){
+						pos["lat"] = val["la"];
+						pos["lng"] = val["lo"];
+						$("#idStation").text(val["n"]);
+						$("#idStation").addClass("colonneCircle");
+						$("#velosDispo").text(val["ba"]);
+						remplirEtatStation("#bloque",val["b"]);
+						$("#bornesDispo").text(val["da"]);
+						remplirEtatStation("#suspendue",val["su"]);
+						$("#velosIndispo").text(val["bx"]);
+						remplirEtatStation("#horsService",val["m"]);
+						$("#borneIndispo").text(val["dx"]);
+					}
+					
+				});
+				if (marker != null){
+					marker.setMap(null);
+				}
+				marker = new google.maps.Marker({
+					position: pos,
+					map: map,
+				});
+			}
+		});
 	});
 	
+	
 };
+
+function remplirEtatStation(colonne,valeur){
+	if (valeur == false){
+		$(colonne).text("Non");
+	}
+	else{
+		$(colonne).text("Oui");
+	}
+}
 
 /*function loadCOntent(file,divId){
 	
@@ -29,7 +71,7 @@ function activateAutoComplete(){
 	}
 */
 function initMap() {
-	var map = new google.maps.Map(document.getElementById('map'), {
+	map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: -34.397, lng: 150.644},
 		zoom: 15
 	});
@@ -49,15 +91,17 @@ function initMap() {
 			}, function() {
             handleLocationError(true, infoWindow, map.getCenter());
 		});
-	} else {
-	// Browser doesn't support Geolocation
-	handleLocationError(false, infoWindow, map.getCenter());
+		} else {
+		// Browser doesn't support Geolocation
+		handleLocationError(false, infoWindow, map.getCenter());
 	}
+	
+	
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-infoWindow.setPosition(pos);
-infoWindow.setContent(browserHasGeolocation ?
-'Error: The Geolocation service failed.' :
-'Error: Your browser doesn\'t support geolocation.');
+	infoWindow.setPosition(pos);
+	infoWindow.setContent(browserHasGeolocation ?
+	'Error: The Geolocation service failed.' :
+	'Error: Your browser doesn\'t support geolocation.');
 }
